c0bf4f8133480f502d7346210ff15f34def5fc81
Surface\marco <marco.forlivesi@outlook.it>
2018-02-18 02:38:23 +0100

    Create gameobject on the terrain

---
+++ Assets/Scripts/CreateGameObject.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateGameObject : MonoBehaviour {
+    public GameObject prefab;
+    public Transform startingPosition;
+    public float force = 10.0f;
+
+	public void OnPress()
+    {
+        GameObject prefabOb = Instantiate(prefab, startingPosition, false);
+        Rigidbody rigidbody = prefabOb.GetComponent<Rigidbody>();
+
+        Vector3 forward = Camera.main.transform.TransformDirection(Vector3.forward);
+        rigidbody.AddForce(forward * force);
+
+    }
+}
---
+++ Assets/Scripts/CreateObjectOnMesh.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateObjectOnMesh : MonoBehaviour {
+    public GameObject prefab;
+    private GameObject prefabObj;
+
+	public void OnTouch()
+    {
+        if (prefabObj == null)
+        {
+            prefabObj = Instantiate(prefab);
+        }
+        else
+        {
+            prefabObj = null;
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (prefabObj)
+        {
+            RaycastHit info;
+            Ray ray = Camera.main.ScreenPointToRay(new Vector2(Screen.width / 2, Screen.height / 2));
+            bool hit = Physics.Raycast(ray, out info, 2000);
+
+            if (hit && info.collider.gameObject != prefabObj)
+            {
+                prefabObj.transform.position = info.point;
+            }
+        }
+	}
+}
